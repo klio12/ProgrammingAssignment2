264681d56435f18eed72fae2d9a2a560b2174d81
klio12 <klio12@yahoo.com>
2017-02-22 01:41:03 -0500

    Reverting back to innitial version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve enable inversion of the input matrix
+## and caching of the result, so that when the same matrix is input to
+## cacheSolve another time, the cached output is retrieved, instead of 
+## having to perform the calculation again
 
-## Write a short comment describing this function
+## function: makeCacheMatrix
+## arguments: matrix
+## output: a list of functions that:
+## 1) return value of matrix (getm)
+## 2) set value of matrix inverse (setsolve)
+## 3) return value of matrix inverse (getsolve)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL
+	setm <- function(y) {
+		x <<- y
+		i <<- NULL
+		}
+	getm <- function() x	
+	setsolve <- function(inv) i <<- inv
+	getsolve <- function() i
+	
+	list(setm = setm, getm = getm, setsolve = setsolve, getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## function: cacheSolve
+## arguments: list returned by makeCacheMatrix
+## output: inverted matrix, either calculated anew or retrieved from cache
+##	(accompanied by a notice)
+## if inverted matrix is created anew, it is passed back to back to
+##	makeCacheMatrix (as input to setsolve)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	i<-x$getsolve()
+	if(!is.null(i)) {
+		message("getting cached data")
+		return(i)
+		}
+	data<-x$getm()
+	i<-solve(data, ...)
+	x$setsolve(i)
+	i
 }
